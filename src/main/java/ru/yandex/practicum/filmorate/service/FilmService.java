cc3cea70f261d075ca2b8496652f3cf4df6ae40f
Ura <alexanoffura1@mail.ru>
2024-02-12 04:30:18 +0300

    обавлена проверка id в removeLike

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -69,7 +69,9 @@ public class FilmService {
     }
 
     public Film removeLike(Long userId, Long filmId) {
-
+        if (!userStorage.containsKey(userId) || !filmStorage.containsKey(filmId)) {
+            throw new NotFoundException("User or friend does not exist");
+        }
         Film film = filmStorage.get(filmId);
         if (!film.getLikes().contains(userId)) {
             return film;
