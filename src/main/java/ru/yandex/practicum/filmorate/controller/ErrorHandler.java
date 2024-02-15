96ee28237f9576ba7ffbf512b7ffbea16c9b3b60
Ura <alexanoffura1@mail.ru>
2024-02-16 00:23:18 +0300

    Вернул метод handleNotFoundException.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.InternalServiceException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.util.Map;
 
@@ -14,6 +16,15 @@ import java.util.Map;
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFoundException(final NotFoundException exception) {
+        log.info("Data not found {}", exception.getMessage());
+        String stacktrace = ExceptionUtils.getStackTrace(exception);
+        return Map.of("Data not found", exception.getMessage(), "Stacktrace: ", stacktrace);
+    }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleInternalServiceException(final InternalServiceException exception) {
