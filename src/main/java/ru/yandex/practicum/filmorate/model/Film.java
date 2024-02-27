b1b56d75ed84b717feaab1027551503139140ee6
Ura <alexanoffura1@mail.ru>
2024-02-27 23:50:57 +0300

    Код оформлен по код стайлу

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -9,9 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Data
 @EqualsAndHashCode(of = {"id"})
