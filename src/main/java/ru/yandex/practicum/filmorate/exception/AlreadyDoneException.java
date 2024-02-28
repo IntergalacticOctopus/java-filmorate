a8f2abfe7d40d033509cffd3cfdc5def50e62a24
Ura <alexanoffura1@mail.ru>
2024-02-29 02:42:59 +0300

    Ошибки исправлены

--- src/main/java/ru/yandex/practicum/filmorate/exception/AlreadyDoneException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class AlreadyDoneException extends RuntimeException {
-    public AlreadyDoneException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -2,11 +2,9 @@ package ru.yandex.practicum.filmorate.model;
 
 public class ErrorResponse {
     private final String error;
-    private final String stacktrace;
 
-    public ErrorResponse(String error, String stacktrace) {
+    public ErrorResponse(String error) {
         this.error = error;
-        this.stacktrace = stacktrace;
     }
 
     public String getError() {
